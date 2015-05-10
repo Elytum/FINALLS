45b5e2494ebf962be7c3f2491d0d7e4c4f1acdf6
Arthur CHAZAL <achazal@e2r1p11.42.fr>
2015-05-10 09:40:05 +0200

    164/241

--- srcs/comparisons.c
+++ srcs/comparisons.c
@@ -12,22 +12,26 @@ int				ft_cmprname(t_file *s1, t_file *s2)
 
 int				ft_cmpdate(t_file *s1, t_file *s2)
 {
-	int			ret;
+	time_t		ret;
 
 	ret = s2->filestat.st_mtime - s1->filestat.st_mtime;
 	if (!ret)
-		ret = ft_strcmp(s1->name, s2->name);
-	return (ret);
+		return (ft_strcmp(s1->name, s2->name));
+	if (ret > 0)
+		return (1);
+	return (-1);
 }
 
 int				ft_cmprdate(t_file *s1, t_file *s2)
 {
-	int			ret;
+	time_t		ret;
 
 	ret = s1->filestat.st_mtime - s2->filestat.st_mtime;
 	if (!ret)
-		ret = ft_strcmp(s2->name, s1->name);
-	return (ret);
+		return (ft_strcmp(s2->name, s1->name));
+	if (ret > 0)
+		return (-1);
+	return (1);
 }
 
 int				ft_cmpsize(t_file *s1, t_file *s2)
